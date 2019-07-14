a6420fba705d9be04f11e062df54246c2345ff83
Austin Meyer <Austin.a.Meyer@gmail.com>
2019-07-14 14:16:01 -0500

    Initial Commit

---
+++ Pointers/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Pointers
+//
+//  Created by Austin Meyer on 7/14/19.
+//  Copyright © 2019 Austin Meyer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
