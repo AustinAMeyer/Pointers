19945a49ddd8bc96f07c19dfd3527e4aca0648c8
Austin Meyer <Austin.a.Meyer@gmail.com>
2019-07-14 14:44:54 -0500

    Initial commit

--- Pointers/main.m
+++ Pointers/main.m
@@ -9,9 +9,31 @@
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
+    
+    /*
+    NSString *fruit = @"Apple";
+    NSLog(@"fruit value: %@ \n", fruit);
+    NSLog(@"fruit address: %p \n", &fruit);
+    */
+    
+    void increment (int *x);
+    
+    int i = 5;
+    NSLog(@"i value: %i \n", i);
+    NSLog(@"i address: %p \n", &i);
+    
+    increment(&i);
+    NSLog(@"i value: %i \n", i);
+    NSLog(@"i address: %p \n", &i);
+    
+   
+    
     return 0;
 }
+
+void increment (int *x){
+    *x = *x +1;
+    NSLog(@"x value: %i \n", *x);
+    NSLog(@"x address: %p \n", x);
+    
+    }
